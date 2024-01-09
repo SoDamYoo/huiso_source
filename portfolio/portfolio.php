53e3d331e766e4a935fd53828c2039a29a544b46
SoDamYoo <45333578+SoDamYoo@users.noreply.github.com>
2024-01-10 02:40:35 +0900

    Add files via upload

---
+++ portfolio/portfolio.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>포트폴리오</title>
+</head>
+<body>
+    <div width="600px">
+        <video auto controls src="<?echo $_REQUEST['name']?>.mp4" style="width: 100%"></video>
+    </div>
+</body>
+</html>
